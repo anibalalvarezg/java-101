6f416ce06ee77877695be97f3bd050a83248e453
Anibal <anibal.alvarezg@gmail.com>
2025-06-20 16:27:54 -0400

    Comparing Objects

--- src/com/inheritance/Main.java
+++ src/com/inheritance/Main.java
@@ -5,6 +5,15 @@ public class Main {
         var control = new UIControl(true);
         var textbox = new TextBox();
         show(control);
+
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
+
+        System.out.println(point1.equals(point1));
+
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
+
     }
 
     public static void show(UIControl control) {
