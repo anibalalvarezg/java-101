ef32659756e8992a53fb1c67547637eb85bd8402
Anibal <anibal@myhotel.cl>
2025-06-19 22:12:29 -0400

    Overloading methods

--- src/Console.java
+++ src/Console.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
 
 public class Console {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+
     public static double readNumber(String prompt, double min, double max) {
-        Scanner sc = new Scanner(System.in);
         double value;
         while (true) {
             System.out.println(prompt);
-            value = sc.nextInt();
+            value = scanner.nextInt();
             if (value >= min && value <= max)
                 break;
             System.out.println("Enter a value between " + min + " and " + max + ": ");
