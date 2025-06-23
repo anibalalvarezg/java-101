64b4a7926fdb83529430a255abd49ef4f56e411d
Anibal <anibal.alvarezg@gmail.com>
2025-06-22 21:52:58 -0400

    Multiples types of exceptions

--- src/com/exceptions/ExceptionsDemo.java
+++ src/com/exceptions/ExceptionsDemo.java
@@ -2,15 +2,21 @@ package com.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
         try {
             var reader = new FileReader("file.txt");
+            var value  = reader.read();
+            new SimpleDateFormat().parse("");
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("File not found");
+        } catch (IOException | ParseException e) {
+            System.out.println("I/O exception");
         }
-
     }
 
     public static void sayHello(String name) {
