d9015e48c0d386530f8b401b9338cbb38f01ba84
Anibal <anibal.alvarezg@gmail.com>
2025-07-02 23:05:51 -0400

    Streams

---
+++ src/com/streams/Main.java
@@ -0,0 +1,25 @@
+package com.streams;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Movie> movies = List.of(
+                new Movie("The Godfather", 10),
+                new Movie("The Godfather: Part II", 11),
+                new Movie("The Godfather: Part III", 112)
+        );
+
+        int count = 0;
+        for (var movie: movies)
+            if (movie.getLikes() > 10)
+                count++;
+
+        var count2 = movies.stream()
+                .filter(movie -> movie.getLikes() > 10)
+                .count();
+
+        System.out.println(count);
+        System.out.println(count2);
+    }
+}
