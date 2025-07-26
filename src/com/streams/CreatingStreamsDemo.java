43bac270eb162c5edf67b8aacb47ac069205a573
Anibal <anibal.alvarezg@gmail.com>
2025-07-25 21:41:47 -0400

    Update streams demo

--- src/com/streams/CreatingStreamsDemo.java
+++ src/com/streams/CreatingStreamsDemo.java
@@ -1,6 +1,7 @@
 package com.streams;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -8,20 +9,20 @@ public class CreatingStreamsDemo {
     public static void main(String[] args) {
         int[] numbers = { 1, 2, 3};
         Arrays.stream(numbers)
-                .forEach(n -> System.out.println(n));
+                .forEach(System.out::println);
 
-        var stream = Stream.generate(() -> Math.random());
+        var stream = Stream.generate(Math::random);
         stream
                 .limit(3)
-                .forEach(n -> System.out.println(n));
+                .forEach(System.out::println);
 
         Stream.iterate(1, n -> n + 1)
                 .limit(10)
-                .forEach(n -> System.out.println(n));
+                .forEach(System.out::println);
 
         var streamList = Stream.of(List.of(1,2,3), List.of(4,5,6));
         streamList
-                .flatMap(list -> list.stream())
+                .flatMap(Collection::stream)
                 .forEach(System.out::println);
     }
 }
