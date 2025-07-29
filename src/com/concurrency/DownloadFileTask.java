aaacd210a3c98719bdadbd8fd21f0e1eed208eef
Anibal <anibal.alvarezg@gmail.com>
2025-07-28 23:25:42 -0400

    Pausing a thread

--- src/com/concurrency/DownloadFileTask.java
+++ src/com/concurrency/DownloadFileTask.java
@@ -4,5 +4,11 @@ public class DownloadFileTask implements Runnable {
     @Override
     public void run() {
         System.out.println("DownloadFileTask.run " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("DownloadFileTask.completed" + Thread.currentThread().getName());
     }
 }
