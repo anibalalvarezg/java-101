4b4340fde729659cf37eb0e4540a79d277729ba9
Anibal <anibal.alvarezg@gmail.com>
2025-07-29 22:26:24 -0400

    Joinning a thread

--- src/com/concurrency/Main.java
+++ src/com/concurrency/Main.java
@@ -7,10 +7,22 @@ public class Main {
         System.out.println(Runtime.getRuntime().availableProcessors());
 
         // Starting a thread
-        System.out.println(Thread.currentThread().getName());
-        for(var i = 1; i <= 10; i++) {
-            Thread thread = new Thread(new DownloadFileTask());
-            thread.start();
+//        System.out.println(Thread.currentThread().getName());
+//        for(var i = 1; i <= 10; i++) {
+//            Thread thread = new Thread(new DownloadFileTask());
+//            thread.start();
+//        }
+
+        Thread thread = new Thread(new DownloadFileTask());
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
+        System.out.println("File is ready to  be scanned");
+
     }
 }
